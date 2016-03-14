42d08141cc26cf88c0f0538ed68d86008817b809
ratherblue <ratherblue@gmail.com>
2016-03-14 15:10:36 -0700

    basic set up

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+# checks that do not have to be enabled
+gem 'xambassador', '0.0.3'
+gem 'sinatra'
+gem 'json'
---
+++ server.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+require 'json'
+
+require 'xambassador/pull_request'
+
+post '/event_handler' do
+  case request.env['HTTP_X_GITHUB_EVENT']
+  when 'pull_request'
+    payload = JSON.parse(request.body.read)
+    Xambassador::PullRequest.new(payload)
+  end
+end
